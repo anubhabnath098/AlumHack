e959ac0b946289d6e0be54e41508bab975e89ff6
Priyam <priyam1234work@gmail.com>
2024-09-25 22:38:58 +0530

    After Menu Addition

--- server/server.js
+++ server/server.js
@@ -1,8 +1,9 @@
 // Import necessary modules
-const express = require('express');
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-const userRoutes = require('./Routes/userRoute'); // Import user routes
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const userRoutes = require("./Routes/userRoute"); // Import user routes
+const menuRoutes = require("./Routes/menuRoute"); // Import menu routes
 
 dotenv.config();
 
@@ -11,21 +12,26 @@ const app = express();
 app.use(express.json());
 
 // MongoDB Connection
-mongoose.connect('mongodb+srv://4:4@cluster2.5lfuw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster2')
-.then(() => console.log("MongoDB connected"))
-.catch(err => console.error("MongoDB connection error:", err));
+mongoose
+  .connect(
+    "mongodb+srv://4:4@cluster2.5lfuw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster2"
+  )
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error("MongoDB connection error:", err));
 
 // Simple route
-app.get('/', (req, res) => {
-    res.send('Welcome to the backend API!');
+app.get("/", (req, res) => {
+  res.send("Welcome to the backend API!");
 });
 
 // Use user routes for registration and login
-app.use('/api/users', userRoutes); // All user-related routes will have a /api/users prefix
+app.use("/api/users", userRoutes); // All user-related routes will have a /api/users prefix
+
+// Use menu routes for adding menu items
+app.use("/api/menu", menuRoutes); // All menu-related routes will have a /api/menu prefix
 
 // Start the server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
-
